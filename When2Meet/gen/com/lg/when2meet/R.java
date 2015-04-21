ce050df5a5c68aad9a66eb925201f5782d365a4a
project <project@LGD-A16>
2015-04-21 15:25:29 +0900

    Implement RoomActivity

--- When2Meet/gen/com/lg/when2meet/R.java
+++ When2Meet/gen/com/lg/when2meet/R.java
@@ -37,17 +37,19 @@ public final class R {
         public static final int main_bar=0x7f020013;
         public static final int room_icon=0x7f020014;
         public static final int room_icon_click=0x7f020015;
-        public static final int test_icon=0x7f020016;
+        public static final int table_border=0x7f020016;
+        public static final int table_selected=0x7f020017;
     }
     public static final class id {
         public static final int LinearLayout1=0x7f070000;
         public static final int add_room=0x7f07000b;
         public static final int calendarView1=0x7f070001;
-        public static final int check_delete=0x7f070017;
+        public static final int check_delete=0x7f07001b;
+        public static final int clear=0x7f070016;
+        public static final int complete=0x7f070015;
         public static final int del_room=0x7f07000c;
         public static final int end_time=0x7f070004;
         public static final int finish_select=0x7f070005;
-        public static final int grid=0x7f070012;
         public static final int join=0x7f070010;
         public static final int join2=0x7f070009;
         public static final int join_name=0x7f070006;
@@ -56,14 +58,17 @@ public final class R {
         public static final int login=0x7f07000f;
         public static final int login_id=0x7f07000d;
         public static final int login_pwd=0x7f07000e;
-        public static final int room_bg=0x7f070013;
-        public static final int room_icon=0x7f070014;
+        public static final int room_bg=0x7f070017;
+        public static final int room_icon=0x7f070018;
         public static final int room_list=0x7f07000a;
-        public static final int room_mem=0x7f070016;
-        public static final int room_name=0x7f070015;
-        public static final int setting=0x7f070011;
+        public static final int room_mem=0x7f07001a;
+        public static final int room_name=0x7f070019;
+        public static final int setting1=0x7f070011;
+        public static final int setting2=0x7f070012;
         public static final int start_time=0x7f070003;
         public static final int storedDates=0x7f070002;
+        public static final int table=0x7f070013;
+        public static final int vote=0x7f070014;
     }
     public static final class layout {
         public static final int activity_create_room=0x7f030000;
@@ -71,7 +76,8 @@ public final class R {
         public static final int activity_list=0x7f030002;
         public static final int activity_login=0x7f030003;
         public static final int activity_room=0x7f030004;
-        public static final int room_list=0x7f030005;
+        public static final int activity_room_vote=0x7f030005;
+        public static final int room_list=0x7f030006;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
@@ -80,6 +86,7 @@ public final class R {
         public static final int title_activity_join=0x7f040003;
         public static final int title_activity_list=0x7f040002;
         public static final int title_activity_room=0x7f040005;
+        public static final int title_activity_room_vote=0x7f040006;
     }
     public static final class style {
         /** 
--- When2Meet/src/com/lg/when2meet/RoomActivity.java
+++ When2Meet/src/com/lg/when2meet/RoomActivity.java
@@ -4,14 +4,22 @@ import java.util.ArrayList;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup.LayoutParams;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.BaseAdapter;
+import android.widget.Button;
 import android.widget.GridView;
 import android.widget.ImageView;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -24,66 +32,83 @@ public class RoomActivity extends Activity {
 		setContentView(R.layout.activity_room);
 
 		Bundle b = this.getIntent().getExtras();
-		String start_time = b.getString("s_time");
-		String end_time = b.getString("e_time");
-		ArrayList<DateClass> datelist = b.getParcelableArrayList("datelist");
-		TextView t = (TextView) findViewById(R.id.setting);
+		final String start_time = b.getString("s_time");
+		int s_time = Integer.parseInt(start_time);
+		final String end_time = b.getString("e_time");
+		final ArrayList<DateClass> datelist = b.getParcelableArrayList("datelist");
+		ArrayList<String> selectedlist = b.getStringArrayList("selectedlist");
+		if(selectedlist==null){
+			selectedlist= new ArrayList<String>();
+		}
+		int time_span = (Integer.parseInt(end_time) - Integer.parseInt(start_time));
+		total_size = datelist.size()* time_span;
 
-		total_size = datelist.size()
-				* (Integer.parseInt(end_time) - Integer.parseInt(start_time));
+		TextView t1 = (TextView) findViewById(R.id.setting1);
+		t1.setText(datelist.get(0).getDate().substring(0, 10)+" ~ "+datelist.get(datelist.size()-1).getDate().substring(0, 10));
+		TextView t2 = (TextView) findViewById(R.id.setting2);
+		t2.setText(start_time+"시 ~ "+end_time+"시 사이에 약속잡기");
 
-		GridView gridview = (GridView) findViewById(R.id.grid);
-		gridview.setAdapter(new ImageAdapter(this));
-		gridview.setNumColumns(datelist.size());
-		gridview.setOnItemClickListener(new OnItemClickListener() {
-			public void onItemClick(AdapterView<?> parent, View v,
-					int position, long id) {
-				Toast.makeText(RoomActivity.this, "" + position,
-						Toast.LENGTH_SHORT).show();
-				ImageView imageView = (ImageView) v;
-				imageView.setImageResource(R.drawable.icon1);
-
+		TableLayout tablelayout = (TableLayout)findViewById(R.id.table);
+//		tablelayout.removeAllViews();
+		for(int i=0; i<time_span+1; i++){
+			TableRow row = new TableRow(this);
+			row.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT));
+			for(int j=0; j<datelist.size()+1; j++){
+				final TextView tv = new TextView(this);
+				tv.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT, TableRow.LayoutParams.WRAP_CONTENT));
+				tv.setGravity(Gravity.CENTER);
+				int pad_size=5;
+				tv.setPadding(pad_size, pad_size, pad_size, pad_size);
+				tv.setBackgroundResource(R.drawable.table_border); 
+				tv.setHighlightColor(0);
+				if(i==0){
+					if(j>0){
+						tv.setText(datelist.get(j-1).getDate().substring(0, 10));
+					}
+				}else{
+					if(j==0){
+						if(s_time<10){
+							tv.setText("0"+(s_time++)+"시");
+						}else{
+							tv.setText((s_time++)+"시");
+						}
+					}else{
+						tv.setText(".");
+						tv.setTextColor(Color.parseColor("#bbeeff"));
+						tv.setHint(datelist.get(j-1).getDate().substring(0, 10)+" "+(Integer.parseInt(start_time)+i-1));
+						
+						boolean check=false;
+						for(int z=0; z<selectedlist.size(); z++){
+							if(selectedlist.get(z).equals(tv.getHint())){
+								check=true;
+							}
+						}
+						if(check){
+							tv.setBackgroundResource(R.drawable.table_selected); 
+						}
+						
+					}
+				}
+				row.addView(tv);
+			}
+			tablelayout.addView(row);
+		}
+		Button button = (Button)findViewById(R.id.vote);
+		button.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Intent i = new Intent(RoomActivity.this, RoomVoteActivity.class);
+				Bundle b = new Bundle();
+				b.putParcelableArrayList("datelist", datelist);
+				b.putString("s_time", start_time);
+				b.putString("e_time", end_time);
+				i.putExtras(b);
+				startActivity(i);
 			}
 		});
+		
 	}
 
-	public class ImageAdapter extends BaseAdapter {
-		private Context mContext;
-
-		public ImageAdapter(Context c) {
-			mContext = c;
-		}
-
-		public int getCount() {
-			return total_size;
-		}
-
-		public Object getItem(int position) {
-			return null;
-		}
-
-		public long getItemId(int position) {
-			return 0;
-		}
-
-		public View getView(int position, View convertView, ViewGroup parent) {
-			ImageView imageView;
-
-			int[] imgs = new int[total_size];
-			for (int i = 0; i < total_size; i++) {
-				imgs[i] = R.drawable.icon2;
-			}
-
-			if (convertView == null) {
-				imageView = new ImageView(mContext);
-				imageView.setLayoutParams(new GridView.LayoutParams(85, 85));
-				imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
-				imageView.setPadding(8, 8, 8, 8);
-			} else {
-				imageView = (ImageView) convertView;
-			}
-			imageView.setImageResource(imgs[position]);
-			return imageView;
-		}
-	}
 }
